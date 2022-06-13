1b77d1e0361e0d38a378c5acceac6da07d0c5039
Tyler Nijmeh <tylernij@gmail.com>
2022-06-13 14:53:58 -0700

    Long press to delete instead of delete button
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- lib/widgets/movie_widget.dart
+++ lib/widgets/movie_widget.dart
@@ -16,23 +16,22 @@ class MovieWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 8, right: 8, top: 8),
-      child: Row(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8),
-              child: Text(movie),
+      child: InkWell(
+        onLongPress: () => onDelete(),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: Text(movie),
+              ),
             ),
-          ),
-          IconButton(
-            onPressed: () => onWatched(),
-            icon: const Icon(Icons.visibility),
-          ),
-          IconButton(
-            onPressed: () => onDelete(),
-            icon: const Icon(Icons.delete),
-          ),
-        ],
+            IconButton(
+              onPressed: () => onWatched(),
+              icon: const Icon(Icons.visibility),
+            ),
+          ],
+        ),
       ),
     );
   }
