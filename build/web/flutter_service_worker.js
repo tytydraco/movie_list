2b0a258ec83d93a8431946bc467d0a53306cde06
Tyler Nijmeh <tylernij@gmail.com>
2022-06-13 14:54:32 -0700

    Update website again
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -18,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "eef709e574cf25dad692760eb8c82358",
-"/": "eef709e574cf25dad692760eb8c82358",
-"main.dart.js": "6256a563cff65b6f1b0817d07fe395d3",
+"index.html": "a7751a0af9b5514be322cf1e75ebc6aa",
+"/": "a7751a0af9b5514be322cf1e75ebc6aa",
+"main.dart.js": "578c0d3c258f3a1e0056f2aa281b1729",
 "manifest.json": "946665e4304343aa5452c1b40abec7dc",
 "version.json": "94b328a79bb586839ec02ab20d54b640"
 };
